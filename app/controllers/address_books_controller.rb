feb5a888ee67fb55f60a335c005f0d22c0fa1c9e
zt <997562971@qq.com>
2015-10-15 14:00:55 -0700

    added json staff

--- app/controllers/address_books_controller.rb
+++ app/controllers/address_books_controller.rb
@@ -6,12 +6,27 @@ class AddressBooksController < ApplicationController
   
   
   respond_to :html, :js
-  # GET /address_books
-  # GET /address_books.json
+  
+  
+  ##-----------------
+  # 如果这里的action的名字xxx与view里xxx.html.erb不一致。需要以下方式说明其关系
+  #respond_to do |format|
+  #    
+  #      format.html { render 'jqtest'}
+  #      format.json { render 'jqtest' }
+  #   
+  #  end
+  ##-----------------
+  
+  # GET /jquery
+  # GET /jquery.json
+  
   def jqueryindex
-	render 'jqtest'
+	@user = current_user
   end
   
+  # GET /address_books
+  # GET /address_books.json
   def index
     @address_books = AddressBook.all
   end
@@ -29,9 +44,22 @@ class AddressBooksController < ApplicationController
   # GET /address_books/1/edit
   def edit
   end
-
+#---------------------------------------
+  #def create
+  #  @new_post=Post.new(post_params)
+  #  if @new_post.save 
+  #  redirect_to '/posts' 
+  #else 
+  #    render 'new' 
+  #end 
+  #end
+  
+  #-----------------------------------------
+  
+  
   # POST /address_books
   # POST /address_books.json
+  
   def create
 	parzt=address_book_params
 	parzt[:auth]=current_user.id
@@ -39,7 +67,7 @@ class AddressBooksController < ApplicationController
 
     respond_to do |format|
       if @address_book.save
-        format.html { redirect_to @address_book, notice: 'Address book was successfully created.' }
+        format.html { redirect_to @address_book, notice: '创建成功！Address book created！' }
         format.json { render :show, status: :created, location: @address_book }
       else
         format.html { render :new }
@@ -53,7 +81,7 @@ class AddressBooksController < ApplicationController
   def update
     respond_to do |format|
       if @address_book.update(address_book_params)
-        format.html { redirect_to @address_book, notice: 'Address book was successfully updated.' }
+        format.html { redirect_to @address_book, notice: '更新完成！Address book updated！' }
         format.json { render :show, status: :ok, location: @address_book }
       else
         format.html { render :edit }
@@ -67,7 +95,7 @@ class AddressBooksController < ApplicationController
   def destroy
     @address_book.destroy
     respond_to do |format|
-      format.html { redirect_to address_books_url, notice: 'Address book was successfully destroyed.' }
+      format.html { redirect_to address_books_url, notice: '删除完成！Address book destroyed！' }
       format.json { head :no_content }
     end
   end
